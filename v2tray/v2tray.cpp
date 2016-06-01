6b2c73f6787d30c7275823c7b3ab770cd50fc416
hyq <i@hyq.me>
2016-06-01 08:27:44 +0800

    将工作目录设置到exe目录

--- v2tray/v2tray.cpp
+++ v2tray/v2tray.cpp
@@ -45,6 +45,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
+	TCHAR szPath[MAX_PATH];
+	GetModuleFileName(NULL, szPath, MAX_PATH);
+	size_t iPos = _tcsrchr(szPath, '\\') - szPath;
+	szPath[iPos] = 0;
+	SetCurrentDirectory(szPath);
 
 	if (!CheckOneInstance())
 	{
